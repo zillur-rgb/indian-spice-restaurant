fa0034255db363aea748a18b0bcf71b8c23e6bff
zillur-rgb <zillurdeu@gmail.com>
2022-11-27 02:29:20 +0100

    Installation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const colors = {
+    brand: {
+      900: '#c0392b',
+      800: '#153e75',
+      700: '#2a69ac',
+    },
+  };
+
+  const theme = extendTheme({ colors });
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
