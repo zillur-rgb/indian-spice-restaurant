944fa37590164790d77a4d6b85fbcd4147586f35
zillur-rgb <zillurdeu@gmail.com>
2022-12-04 20:21:46 +0100

    fin-3

--- pages/speisekarte.tsx
+++ pages/speisekarte.tsx
@@ -30,7 +30,12 @@ export default function speisekarte() {
         bg="brand.900"
         flexDir={{ base: 'column', lg: 'row' }}
       >
-        <Box w="50%" h="70vh" overflow={'hidden'} bg="brand.900">
+        <Box
+          w={{ base: '100%', lg: '50%' }}
+          h="70vh"
+          overflow={'hidden'}
+          bg="brand.900"
+        >
           <iframe
             src="/pdfs/aussen.pdf"
             title="testPdf"
@@ -38,7 +43,12 @@ export default function speisekarte() {
             width="100%"
           />
         </Box>
-        <Box w="50%" h="70vh" overflow={'hidden'} bg="brand.900">
+        <Box
+          w={{ base: '100%', lg: '50%' }}
+          h="70vh"
+          overflow={'hidden'}
+          bg="brand.900"
+        >
           <iframe
             src="/pdfs/innen.pdf"
             title="testPdf"
